2faa421b731485e07d7c79612bbb9539c0a9ff36
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-06-13 12:35:07 +0100

    FEAT: added the skeleton for other admin dashboard pages

---
+++ app/admin/logistics/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminLogisticsPage() {
+  return (
+    <main>
+      <h1>This is the logistics page for the admin usage</h1>
+    </main>
+  );
+}
---
+++ app/admin/matching/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminMatchingPage() {
+  return (
+    <main>
+      <h1>This page is where the admin matches request to donations</h1>
+    </main>
+  );
+}
